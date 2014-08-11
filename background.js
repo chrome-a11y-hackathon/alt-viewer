2104517bf910a4c0475286466b25ebd6a1461d1f
Alice Boxhall <aboxhall@chromium.org>
2014-08-11 11:38:26 -0700

    Basic pure-CSS version working

--- background.js
+++ background.js
@@ -5,8 +5,9 @@
 chrome.commands.onCommand.addListener(function(command) {
   console.log('command: ', command);
   if (command == 'example_keyboard_command') {
-    chrome.tabs.executeScript({
-      code: 'document.body.style.backgroundColor="red"'
-    });
+    chrome.tabs.insertCSS({
+      file: 'hide-images.css',
+      allFrames: true
+    }, function() { console.log('successfully injected'); });
   }
 });
