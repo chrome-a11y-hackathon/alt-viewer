71be4aafbf43b2fcd8fa13e57b45f7461052fa45
Alice Boxhall <aboxhall@chromium.org>
2014-08-11 17:05:59 -0700

    Tweaks to style to aid readability; added new icon for missing alt text

--- background.js
+++ background.js
@@ -2,16 +2,28 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.commands.onCommand.addListener(function(command) {
-  console.log('command: ', command);
-  if (command == 'example_keyboard_command') {
-    chrome.tabs.executeScript({
-      file: 'hide-images.js',
-      allFrames: true
-    }, function() { console.log('successfully injected script'); });
+function injectScripts(opt_tab) {
+  chrome.tabs.executeScript({
+    file: 'hide-images.js',
+    allFrames: true
+  }, function() {
+    console.log('successfully injected script', opt_tab ? opt_tab.url : '');
     chrome.tabs.insertCSS({
       file: 'hide-images.css',
       allFrames: true
-    }, function() { console.log('successfully injected css'); });
+    }, function() {
+      console.log('successfully injected css', opt_tab ? opt_tab.url : '');
+    });
+  });
+}
+chrome.commands.onCommand.addListener(function(command) {
+  console.log('command: ', command);
+  if (command == 'example_keyboard_command') {
+    injectScripts();
   }
 });
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  injectScripts(tab);
+});
+
