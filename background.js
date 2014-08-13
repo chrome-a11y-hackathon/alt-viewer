c9279005e5f6517801ef68bc79fe0ede0f6483e8
Alice Boxhall <aboxhall@chromium.org>
2014-08-12 17:29:21 -0700

    Showing status bar

--- background.js
+++ background.js
@@ -4,18 +4,34 @@
 
 function injectScripts(opt_tab) {
   chrome.tabs.executeScript({
-    file: 'hide-images.js',
+    file: 'lib/axs_testing.js',
     allFrames: true
   }, function() {
-    console.log('successfully injected script', opt_tab ? opt_tab.url : '');
-    chrome.tabs.insertCSS({
-      file: 'hide-images.css',
+    console.log('successfully injected axs_testing.js');
+    chrome.tabs.executeScript({
+      file: 'hide-images.js',
       allFrames: true
     }, function() {
-      console.log('successfully injected css', opt_tab ? opt_tab.url : '');
+      console.log('successfully injected script', opt_tab ? opt_tab.url : '');
+      chrome.tabs.insertCSS({
+        file: 'hide-images.css',
+        allFrames: true
+      }, function() {
+        console.log('successfully injected css', opt_tab ? opt_tab.url : '');
+        chrome.tabs.executeScript({
+          code: 'toggleEnabled();',
+          allFrames: true
+        }, function() {
+          console.log('created infobar');
+          chrome.tabs.executeScript({
+            code: 'createInfobar();'
+          });
+        });
+      });
     });
   });
 }
+
 chrome.commands.onCommand.addListener(function(command) {
   console.log('command: ', command);
   if (command == 'example_keyboard_command') {
